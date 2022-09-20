1927c9715c9551abc413e0197e9b854a8da26aba
YNCBearz <bear19930414@gmail.com>
2022-09-20 15:46:58 +0800

    add ObjectHelper for Dump

---
+++ CSharpBasic/Utils/ObjectHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace CSharpBasic.Utils
+{
+    public static class ObjectHelper
+    {
+        public static void Dump<T>(this T x)
+        {
+            string json = JsonConvert.SerializeObject(x, Formatting.Indented);
+            Console.WriteLine(json);
+        }
+    }
+}
